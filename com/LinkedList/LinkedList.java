7b9f1902ea4784b08cc001541f5bde3b2e14ed0b
smrutikapatil <smruti.patil17@gmail.com>
2022-01-21 14:20:18 +0530

    Added- Create linked list of 56, 30 and 70

--- com/LinkedList/LinkedList.java
+++ com/LinkedList/LinkedList.java
@@ -1,7 +1,28 @@
 package com.LinkedList;
 
-public class LinkedList {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List");
+public class LinkedList<K> {
+	public INode<K> head;
+	public INode<K> tail;
+
+	public LinkedList() {
+
 	}
+
+	public void add(INode<K> newNode) {
+		if (this.tail == null) {
+			this.tail = newNode;
+		}
+		if (this.head == null) {
+			this.head = newNode;
+		} else {
+			INode<K> tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	public void printMyNodes() {
+		System.out.println("My Node" + head);
+	}
+
 }
