16b7f3be7b88443c21ef7a952500cf4ca862760d
smrutikapatil <smruti.patil17@gmail.com>
2022-01-22 18:41:30 +0530

    Added- Ability to delete first element in linked list

--- com/LinkedList/LinkedList.java
+++ com/LinkedList/LinkedList.java
@@ -40,6 +40,12 @@ public class LinkedList<K> {
 		newNode.setNext(tempNode);
 	}
 
+	public INode pop() {
+		INode tempNode = this.head;
+		this.head = head.getNext();
+		return tempNode;
+	}
+
 	public void printMyNode() {
 		System.out.println(head);
 	}
--- com/LinkedList/MyNodeTest.java
+++ com/LinkedList/MyNodeTest.java
@@ -5,14 +5,15 @@ import org.junit.Test;
 
 public class MyNodeTest {
 	@Test
-	public void given3NumberWhenInsertingSecondInBetweenShouldPassLinkedListResult() {
+	public void givenFirstElementWhenDeletedShouldPassLinkedListResult() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
 		LinkedList linkedlist = new LinkedList();
 		linkedlist.add(myFirstNode);
+		linkedlist.append(mySecondNode);
 		linkedlist.append(myThirdNode);
-		linkedlist.insert(myFirstNode, mySecondNode);
+		linkedlist.pop();
 		linkedlist.printMyNode();
 
 		boolean result = linkedlist.head.equals(myFirstNode) && linkedlist.head.getNext().equals(mySecondNode)
